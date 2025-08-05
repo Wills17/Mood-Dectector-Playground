b9cfddbd583f8ad1f17f549b379e0265105847c6
Williams Odunayo <williamsodunayo70@gmail.com>
2025-08-05 08:37:17 +0100

    Import libraires; Add data path.

--- model_train.py
+++ model_train.py
@@ -1,8 +1,14 @@
-
 # import libraries
-import kagglehub
+import os
+import cv2 as cv
+import numpy as np
+# from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import time
+from tqdm import tqdm
 
-# Download latest version of dataset
-path = kagglehub.dataset_download("jonathanoheix/face-expression-recognition-dataset")
 
-print("Path to dataset files:", path)
+# initializes list for images and label
+data_train = "Face Expression Recognition Dataset/images/train/"
+data_validate = "Face Expression Recognition Dataset/images/train/"
