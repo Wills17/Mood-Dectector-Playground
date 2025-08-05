5e31ca9b87403e752b7fbe4808bd39c3f1cf905a
Williams Odunayo <williamsodunayo70@gmail.com>
2025-08-05 08:57:21 +0100

    Restructure dataset; Read and convert images to grayscale.

--- model_train.py
+++ model_train.py
@@ -10,5 +10,40 @@ from tqdm import tqdm
 
 
 # initializes list for images and label
-data_train = "Face Expression Recognition Dataset/images/train/"
-data_validate = "Face Expression Recognition Dataset/images/train/"
+data_train = "Face Expression Recognition Dataset/train/"
+data_validate = "Face Expression Recognition Dataset/validation/"
+
+# List for arrays
+X_train = []
+y_train = []
+folders = ['surprise', 'disgust', 'happy', 'sad', 'fear', 'neutral', 'angry']
+
+
+# Load and append dataset for preprocessing
+# Train folder
+for folder in folders:
+    train_path = data_train + folder
+    print("\nCurrent folder:", folder)
+    for current_img in tqdm(os.listdir(train_path)):
+        
+        # Read and convert image to Grayscale
+        img = cv.imread(train_path + "/" + current_img, cv.IMREAD_GRAYSCALE)
+        
+        # Display image
+        cv.imshow(f"Original Image {current_img}", img)
+        cv.waitKey(1000)
+        
+        # resize images
+        img = cv.resize(img, (250, 250))
+        
+        # Show resized imaage
+        cv.imshow(f"Resized Image {current_img}", img)
+        cv.waitKey(1000)
+        cv.destroyAllWindows()
+        
+        X_train.append(img)
+        y_train.append(folder)
+        
+    print("Appended all images in {} folder successfully.".format(folder))
+    
+print(f"\nAppended all images in the Training folder successfully.")
