be6aef4ece2b85453d4c99fa681abfcdf0f73751
Williams Odunayo <williamsodunayo70@gmail.com>
2025-08-05 07:08:47 +0100

    Download face esxpression dataset.

---
+++ model_train.py
@@ -0,0 +1,8 @@
+
+# import libraries
+import kagglehub
+
+# Download latest version of dataset
+path = kagglehub.dataset_download("jonathanoheix/face-expression-recognition-dataset")
+
+print("Path to dataset files:", path)
